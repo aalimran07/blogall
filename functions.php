74909d34bf171a95fcd75de002a65898f37274bd
wp-expert <tfthemecafe@gmail.com>
2018-04-12 16:45:25 +0600

    I have created some js file and some js scripts

--- functions.php
+++ functions.php
@@ -183,6 +183,7 @@ if ( defined( 'JETPACK__VERSION' ) ) {
  */
 function blogall_admin_scripts() {
 	wp_enqueue_style('blogall-admin-custom', get_template_directory_uri() . '/css/admin/custom.css');
-	wp_enqueue_script( 'blogall-admin-custom', get_template_directory_uri() . '/js/admin/custom.js' );
+	 wp_enqueue_media();
+	wp_enqueue_script( 'blogall-admin-custom', get_template_directory_uri() . '/js/admin/custom.js', array('jquery'), null, true );
 }
 add_action('admin_enqueue_scripts', 'blogall_admin_scripts');
---
+++ js/admin/custom.js
@@ -0,0 +1,14 @@
+(function($){
+
+$(document).on('click', 'button#about_img', function(e){
+	e.preventDefault();
+
+	var button = $(this);
+
+	var file_frame = wp.media.frames.file_frame = wp.media({
+
+	});
+});
+
+
+}(jquery));
